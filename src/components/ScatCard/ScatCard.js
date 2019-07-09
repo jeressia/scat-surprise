e81c82febc3f7d245b9b7601ac79a56b89a6222f
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-07-08 20:39:25 -0500

    made propz

--- src/components/ScatCard/ScatCard.js
+++ src/components/ScatCard/ScatCard.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import scatShape from '../../helpers/data/propz/scatShape';
 
 class ScatCard extends React.Component {
+  static propTypes = {
+    scat: scatShape.scatCardShape,
+  };
+
   render() {
     const { scat } = this.props;
     return (
       <div className="ScatCard col-4">
         <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{scat.sampleName}</h5>
-              <p className="card-text">{scat.color}</p>
-              {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-            </div>
-</div>
+          <div className="card-body">
+            <h5 className="card-title">{scat.sampleName}</h5>
+            <p className="card-text">{scat.color}</p>
+            {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+          </div>
         </div>
+      </div>
     );
   }
 }
